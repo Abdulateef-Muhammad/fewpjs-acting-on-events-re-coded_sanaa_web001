a1746fcaccf7baf44561b6302329d408519b8ba8
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-07-23 13:21:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace('px', '');
     let left = parseInt(leftNumbers, 10);
     if(left > 0 ) {
-        dodger.style.left = `${left - 5}px`;
+        dodger.style.left = `${left - 1}px`;
     }
 }
 
 function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace('px', '');
   let left = parseInt(leftNumbers, 10);
-  if(left > 0 ) {
+  if(left  < 360 ) {
       dodger.style.left = `${left + 1}px`;
   }
 }
